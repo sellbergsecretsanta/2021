071543beb67c87f05be3c0d74e2bc4b212728f99
Erik Götesson <erik_gotesson@hotmail.com>
2019-05-08 15:54:37 +0200

    Updates

---
+++ precache-manifest.1e5a930e80e35d1715c1ea8360becf33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e233ff098566ee1fccd6280a9d3d85",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "82995732ee339f64fb2c",
+    "url": "/react-gh-pages/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-gh-pages/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "82995732ee339f64fb2c",
+    "url": "/react-gh-pages/static/js/main.750bcd0f.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
