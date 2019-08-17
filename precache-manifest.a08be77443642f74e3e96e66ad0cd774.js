07bd211aabb30a885032f64021fa36e3f2a32ce4
Erik Götesson <erik_gotesson@hotmail.com>
2019-08-18 01:01:12 +0200

    Updates

---
+++ precache-manifest.a08be77443642f74e3e96e66ad0cd774.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca677e83954aff6318a1bf261aeaff6",
+    "url": "/classic/index.html"
+  },
+  {
+    "revision": "7ad60c3d3541d8965e44",
+    "url": "/classic/static/css/main.c50f064a.chunk.css"
+  },
+  {
+    "revision": "c73c0cd1517f2b7beca9",
+    "url": "/classic/static/js/2.83cbe99f.chunk.js"
+  },
+  {
+    "revision": "7ad60c3d3541d8965e44",
+    "url": "/classic/static/js/main.cd7b0ff2.chunk.js"
+  },
+  {
+    "revision": "8b434b4dd76220f02418",
+    "url": "/classic/static/js/runtime~main.4317a9a1.js"
+  }
+]);
